be4c3e269b24110dc23c389e46dcb16bc8bf2402
Merlyn Albery-Speyer <curious.attempt.bunny@gmail.com>
2011-01-23 17:14:02 -0800

    made Example.groovy repeatable

--- src/example/groovy/Example.groovy
+++ src/example/groovy/Example.groovy
@@ -1,7 +1,7 @@
 import com.curiousattemptbunny.grouch.*
 
 grouch = new Grouch('http://localhost:5984')
-grouch.mydb.delete()
+if (grouch.mydb.exists()) grouch.mydb.delete()
 grouch.mydb.create()
 grouch.mydb << [_id: 'mydoc', aField: 'aValue' ]
 assert grouch.mydb.mydoc.aField == 'aValue'
